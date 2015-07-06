e932520b35302ac9298a2393dd4abee2936dad5f
Marek Karmelski <marek.karmelski@gmail.com>
2015-07-06 12:26:41 +0200

    Add two files to js folder

---
+++ js/css-scroller-1.0.js
@@ -0,0 +1,70 @@
+// JavaScript Document
+(function () {
+	
+	// get all element with 'data-fade' attribute
+	function getFadeElement() {
+		
+		var fadeElements = $("[data-fade=active]");
+		var elements = [];
+		var i = 0;
+	
+		fadeElements.each(function() {
+			$(this).removeClass($(this).attr("data-after-class"));
+			$(this).addClass($(this).attr("data-before-class"));
+			
+			var element = [];
+			element[0] = this;
+			element[1] = ($(this).offset().top - $(window).height()) + ($(window).height() * parseFloat($(this).attr("data-start-display")));
+			element[2] = $(this).attr("data-before-class");
+			element[3] = $(this).attr("data-after-class");
+			
+			elements[i] = element;
+			
+			i++;
+		});
+		
+		return elements;
+		
+	};
+	
+	// init and display elements
+	function initFadeElements(elements) {
+		
+		var scrollTop = $(window).scrollTop();
+		var arrayLength = elements.length;
+		
+		for ( var x = 0; x < arrayLength; x++ ) {
+			if ( ( elements[x][1] <= 0 ) || ( scrollTop > elements[x][1] ) ) {
+				$(elements[x][0]).addClass(elements[x][3]);
+			} else {
+				$(elements[x][0]).removeClass(elements[x][3]);
+			}
+		}
+	}
+	
+	$(document).ready(function() {
+		
+		var elements = getFadeElement();
+		var arrayLength = elements.length;
+		
+		initFadeElements(elements);
+		
+		$(window).resize(function() {
+			elements = getFadeElement();
+			arrayLength = elements.length;
+			initFadeElements(elements);
+		});
+	
+		$(window).scroll(function() {
+			var scrollTop = $(window).scrollTop();
+			for ( var x = 0; x < arrayLength; x++ ) {
+				if (scrollTop > elements[x][1]) {
+					$(elements[x][0]).addClass(elements[x][3]);
+				} else {
+					$(elements[x][0]).removeClass(elements[x][3]);
+				}
+			}
+		});	
+	});
+	
+}).call(this);
