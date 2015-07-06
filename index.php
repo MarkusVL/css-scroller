21bc63e07869afec6705de579d856923bf2d410d
Marek Karmelski <marek.karmelski@gmail.com>
2015-07-06 12:43:57 +0200

    Add demo index.php file

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Demo page: css-scroller</title>
+<script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<div class="block" id="block1">
+    </div>
+    <div class="block" id="block2">
+    	<span class="fade on1" data-fade="active" data-before-class="off1" data-after-class="on1" data-start-display="0.2">SHOW</span>
+        <span class="fade on" data-fade="active" data-before-class="off" data-after-class="on" data-start-display="0.2">SHOW</span>
+    </div>
+    <div class="block" id="block3">
+    	<span class="fade on" data-fade="active" data-before-class="off" data-after-class="on" data-start-display="0.2">Lorem ipsum dolor sit amet neque lorem, iaculis odio, sagittis lorem. Nam pharetra vel, purus. Fusce ullamcorper. Nam vestibulum. Nunc elementum. Aenean lacus lacus vehicula enim sed eros ac eros. Ut bibendum id, porttitor egestas. Proin gravida eros quis libero. Vestibulum ante ipsum orci, blandit malesuada. Etiam eu libero. Duis at magna. Integer est. Pellentesque habitant morbi tristique mauris nec dui. Ut sed enim diam, venenatis consequat. Quisque in dolor. Pellentesque euismod id, libero. Aliquam tempus malesuada. Etiam ornare nisl, tristique eget, eros. In urna. Sed in interdum ligula condimentum auctor vitae, imperdiet sed, congue dolor. Integer est. Sed nec augue. Vestibulum quam ultricies tortor in dolor. Pellentesque dolor leo non dui. Aenean aliquet. Praesent tortor venenatis eu, pede. Pellentesque tempus enim interdum adipiscing elit. In urna. Donec condimentum ultricies, velit nulla ac nibh lacus, congue ac, semper sed, sollicitudin mi leo sed adipiscing elit. Nam id nibh. Donec in augue. Maecenas facilisis vel, purus. Integer mi libero, fringilla gravida. Suspendisse sed est. Aliquam erat sit amet sapien aliquet lacinia neque. Vestibulum ante ipsum primis in lorem quam leo, aliquet commodo nec, dictum enim. Cras mattis, magna ut diam. Sed et velit. Suspendisse sed condimentum urna, id lectus. Pellentesque varius nec, sagittis mi. Pellentesque malesuada eros, sagittis venenatis. Curabitur eget sapien. Etiam lobortis mauris lacus iaculis ut, lectus. Nunc eleifend mauris lacus tincidunt fermentum. Morbi felis a urna. Proin justo. Donec nunc. Fusce iaculis et, tincidunt augue eget lectus. Ut a condimentum ultricies, hendrerit wisi. Aenean quis velit. Cras ut leo luctus nulla ultricies tincidunt, risus eu diam. Phasellus vestibulum dictum accumsan id, semper risus commodo est. Aliquam ut sapien magna ante, cursus justo facilisis pellentesque sed, congue et, accumsan. Lorem ipsum dolor sit amet neque lorem, iaculis odio, sagittis lorem. Nam pharetra vel, purus. Fusce ullamcorper. Nam vestibulum. Nunc elementum. Aenean lacus lacus vehicula enim sed eros ac eros. Ut bibendum id, porttitor egestas. Proin gravida eros quis libero. Vestibulum ante ipsum orci, blandit malesuada. Etiam eu libero. Duis at magna. Integer est. Pellentesque habitant morbi tristique mauris nec dui. Ut sed enim diam, venenatis consequat. Quisque in dolor. Pellentesque euismod id, libero. Aliquam tempus malesuada. Etiam ornare nisl, tristique eget, eros. In urna. Sed in interdum ligula condimentum auctor vitae, imperdiet sed, congue dolor. Integer est. Sed nec augue. Vestibulum quam ultricies tortor in dolor. Pellentesque dolor leo non dui. Aenean aliquet. Praesent tortor venenatis eu, pede. Pellentesque tempus enim interdum adipiscing elit. In urna. Donec condimentum ultricies, velit nulla ac nibh lacus, congue ac, semper sed, sollicitudin mi leo sed adipiscing elit. Nam id nibh. Donec in augue. Maecenas facilisis vel, purus. Integer mi libero, fringilla gravida. Suspendisse sed est. Aliquam erat sit amet sapien aliquet lacinia neque. Vestibulum ante ipsum primis in lorem quam leo, aliquet commodo nec, dictum enim. Cras mattis, magna ut diam. Sed et velit. Suspendisse sed condimentum urna, id lectus. Pellentesque varius nec, sagittis mi. Pellentesque malesuada eros, sagittis venenatis. Curabitur eget sapien. Etiam lobortis mauris lacus iaculis ut, lectus. Nunc eleifend mauris lacus tincidunt fermentum. Morbi felis a urna. Proin justo. Donec nunc. Fusce iaculis et, tincidunt augue eget lectus. Ut a condimentum ultricies, hendrerit wisi. Aenean quis velit. Cras ut leo luctus nulla ultricies tincidunt, risus eu diam. Phasellus vestibulum dictum accumsan id, semper risus commodo est. Aliquam ut sapien magna ante, cursus justo facilisis pellentesque sed, congue et, accumsan.</span>
+    </div>
+    <div class="block" id="block4">
+    	<span class="fade on" data-fade="active" data-before-class="off" data-after-class="on" data-start-display="0.2">SHOW</span>
+    </div>
+    <div class="block" id="block5">
+    	<span class="fade on" data-fade="active" data-before-class="off" data-after-class="on" data-start-display="0.2">SHOW</span>
+    </div>
+    <div class="block" id="block6">
+    	<span class="fade on" data-fade="active" data-before-class="off" data-after-class="on" data-start-display="0.2">SHOW</span>
+    </div>
+    <div class="block" id="block7">
+    	<span class="fade on" data-fade="active" data-before-class="off" data-after-class="on" data-start-display="0.2">SHOW</span>
+    </div>
+    <div class="block" id="block8">
+    	<span class="fade on" data-fade="active" data-before-class="off" data-after-class="on" data-start-display="0.10">SHOW</span>
+    </div>
+</body>
+<script src="js/css-scroller-1.0.js" type="text/javascript" ></script>
+</html>
